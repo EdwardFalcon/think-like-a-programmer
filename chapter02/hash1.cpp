9737c440a81983186bef8c78d586adab6a77015d
EdwardFalcon <devedwardfalcon@gmail.com>
2022-10-05 00:13:24 +0300

    add some examples from chapter 1

---
+++ chapter02/hash1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    for (int hashNum = 1; hashNum <= 5; hashNum++)
+    {
+        cout << "#";
+    }
+    cout << "\n";
+    return 0;
+}
---
+++ chapter02/hash4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    for (int row = 1; row <= 7; row++)
+    {
+        for (int hashNum = 1; hashNum <= 4 - abs(4 - row); hashNum++)
+        {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
